3354ff8b1700513a0443efc26ba1816f72800c86
Denis Strizhkin <strdenis02@gmail.com>
2023-02-28 22:47:01 +0300

    lab2 project structure

---
+++ lab2/Matrix.cs
@@ -0,0 +1,5 @@
+namespace lab2; 
+
+public class Matrix {
+    
+}
---
+++ lab2/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace lab2; 
+
+internal abstract class Program {
+    public static void Main(string[] args) {
+        Console.WriteLine("hello");
+    }
+}
